f6e6c9581a1d5f97fdcb6e50880002016cffba63
karlos g liberal (patxangas) <karlos@investic.net>
2012-06-13 00:08:11 +0200

    callback en la funcion asincrona de fluidinfo

--- public/javascripts/drupal.js
+++ public/javascripts/drupal.js
@@ -1,6 +1,16 @@
 var Drupal = function(){
 
-  datos = {}; 
+  datos = {};
+  datos.vocabularioNodos = [
+    "elfilo.net/drupalblog/Audio_vídeo_interacción", 
+    "elfilo.net/drupalblog/Cacharreo_Aprendizaje_Hacking_Diy", 
+    "elfilo.net/drupalblog/Dispositivos_móviles", 
+    "elfilo.net/drupalblog/Fabricación_digital"
+  ];
+
+  datos.objetoFluidNodos = "interzonas.elfilo.net:vocabulary:nodos";
+
+  datos.todos;
   datos.nodos = function(){
     var tags = ["hola", "hola2"];
     return tags;
@@ -15,14 +25,14 @@ var Drupal = function(){
   };
 
   datos.totales = function(){
-    console.log(this.nodos());
-    var total = {"clave": "valor"};
-    return total;
+    acceso_fluidinfo(function(tt){
+     datos.todos =  tt.data.tagNames;
+    });
+    return datos.todos;
   }
 
-  var acceso_fluidinfo = function(){
+  var acceso_fluidinfo = function(cb){
     var resultados = "res";
-    var nodos = [];
     var session_anonima = {}
     var session = fluidinfo(session_anonima);
     var options = {
@@ -31,16 +41,12 @@ var Drupal = function(){
         returnDescription: true,
         returnTags: true
       },
-      onSuccess: function(result) {console.log(result);},
+      onSuccess: function(result) {
+        cb(result)
+      },
       onError: function(result) { /* handle any problems */ }
    };
-   console.log(session.api.get(options));
-
-    this.nodos = function(){
-      return "nodos";
-    }
-
-    return resultados;
+   session.api.get(options);
   }
 
   return datos;
