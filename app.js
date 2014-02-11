708ef1f290af2ab993ce0babe0f9d2771b2f5f4d
karlosgliberal <karlosgliberal@gmail.com>
2014-02-11 10:34:43 +0100

    cambio de puerto

--- app.js
+++ app.js
@@ -49,5 +49,5 @@ app.get('/eu', function(req, res){
 // app.get('/', routes.index);
 // app.get('/eu/', routes.eu);
 
-app.listen(3000);
+app.listen(4200);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
